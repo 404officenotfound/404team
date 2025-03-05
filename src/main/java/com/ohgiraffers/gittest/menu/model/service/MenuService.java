1e19ce818e91ad77a909220700f3d419190c9491
YYSSJJIINN <h20171058@daum.net>
2025-03-05 17:12:34 +0900

    [feat] main 브랜치 기본틀 완성

---
+++ src/main/java/com/ohgiraffers/gittest/menu/model/service/MenuService.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.gittest.menu.model.service;
+
+import com.ohgiraffers.gittest.menu.model.dao.MenuMapper;
+import com.ohgiraffers.gittest.menu.model.dto.CategoryDTO;
+import com.ohgiraffers.gittest.menu.model.dto.MenuDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuService {
+
+    private final MenuMapper menuMapper;
+
+    @Autowired
+    public MenuService(MenuMapper menuMapper) {
+
+        this.menuMapper = menuMapper;
+    }
+
+
+}
